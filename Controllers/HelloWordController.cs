a49df7e395f824ded309c5e5c7e281a011d8bf8c
aludcar <aludcar@gmail.com>
2021-07-21 21:29:17 -0400

    asp-net-core

---
+++ Controllers/HelloWordController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers {
+    public class HelloWordController: Controller{
+        //GET: /HelloWord/
+        public IActionResult Index() {
+            return View();
+        }
+
+        //GET: /HelloWord/Welcome/
+        public IActionResult Welcome(string name, int numTimes = 1) {
+            ViewData["Message"] = $"Hello {name}";
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
